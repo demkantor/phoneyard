a4fc8802a4ad74124af3a39cf83d4bfce599b891
demkantor <demkantor@gmail.com>
2020-08-08 09:06:01 -0500

    added styles to product cards

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,13 +4,60 @@ import { Link } from 'react-router-dom';
 import { ProductContext } from '../../context/context';
 
 const ProductWrapper = styled.div`
-
+    .card {
+        border-color: transparent;
+        transition: var(--tran-secondary);
+    }
+    .card-footer {
+        background: transparent;
+        border-top: transparent;
+        transition: var(--tran-secondary);
+    }
+    &:hover {
+        .card {
+            border: 0.04rem solid rgba(0,0,0,0.2);
+            box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2);
+        }
+        .card-footer {
+            background: rgba(247, 247, 247);
+        }
+    }
+    .img-container {
+        position: relative;
+        overflow: hidden;
+    }
+    .card-img-top {
+        transition: var(--tran-secondary);
+    }
+    .img-container:hover .card-img-top {
+        transform: scale(1.2);
+    }
+    .cart-btn {
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        padding: 0.2rem 0.4rem;
+        background: var(--blue-secondary);
+        border: none;
+        color: var(--white-primary);
+        font-size: 1.4rem;
+        border-radius: 0.5rem 0 0 0;
+        transform: translate(100%, 100%);
+        transition: var(--tran-secondary);
+    }
+    .img-container:hover .cart-btn {
+        transform: translate(0,0);
+    }
+    .cart-btn:hover {
+        color: var(--blue-primary);
+        cursor: pointer;
+    }
 `;
 
 
 const Product = ({ product }) => {
-    const { id, title, img, price, inCart } = product;
-    const { products, handleDetails, addToCart } = useContext(ProductContext);
+    const { id, img, inCart, price, title } = product;
+    const { addToCart, handleDetails } = useContext(ProductContext);
 
     return (
         <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
