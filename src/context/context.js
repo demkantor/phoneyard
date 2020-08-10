dd992e24de0080dfe4530d9e620f6c81388372c2
demkantor <demkantor@gmail.com>
2020-08-10 17:03:18 -0500

    now can remove one item at a time

--- src/context/context.js
+++ src/context/context.js
@@ -5,7 +5,7 @@ const ProductContext = createContext();
 
 
 const ProductProvider = ({ children }) => {
-    const [cart, setCart] = useState(storeProducts);
+    const [cart, setCart] = useState([]);
     const [cartSubtotal, setCartSubtotal] = useState(0);
     const [cartTax, setCartTax] = useState(0);
     const [cartTotal, setCartTotal] = useState(0);
@@ -16,34 +16,41 @@ const ProductProvider = ({ children }) => {
 
 
     useEffect(() => {
-        const setOurProducts = () => {
-            let ourProducts = [];
-            storeProducts.forEach((item) => {
-                const singleItem = {...item};
-                ourProducts = [...ourProducts, singleItem];
-            });
-            setProducts(ourProducts);
-        };
-
         setOurProducts();
+
     }, []);
 
-    const addToCart = (id) => {
+    const addToCart = async (id) => {
         let tempProducts = [...products];
-        const index = tempProducts.indexOf(getItem(id));
-        const product = tempProducts[index];
+        const index = await tempProducts.indexOf(getItem(id));
+        const product = await tempProducts[index];
 
         product.inCart = true;
         product.count = 1;
         const price = product.price;
         product.total = price;
 
-        setProducts(tempProducts);
-        setCart([...cart, product]);
+        await setProducts(tempProducts);
+        await setCart([...cart, product]);
+        await addTotals();
     };
 
+    const addTotals = async () => {
+        let subtotal = 0;
+        await cart.map(item =>(subtotal += item.total));
+        /////////////////// set tax rate here ///////////current = 0.075///////////////
+        const tempTax = subtotal * 0.075;
+        const tax = parseFloat(tempTax.toFixed(2));
+        const total = subtotal + tax;
+        await setCartSubtotal(subtotal);
+        await setCartTax(tax);
+        await setCartTotal(total);
+    }
+
     const clearCart = () => {
-        console.log('cart emptied!');
+        setCart([]);
+        setOurProducts();
+        addTotals();
     };
 
     const closeModal = () => {
@@ -75,7 +82,28 @@ const ProductProvider = ({ children }) => {
     };
 
     const removeItem = (id) => {
-        console.log('remove item', id)
+        let tempProducts = [...products];
+        let tempCart = [...cart];
+
+        tempCart = tempCart.filter(item => item.id !== id);
+        const index = tempProducts.indexOf(getItem(id));
+        let removedProduct = tempProducts[index];
+        removedProduct.inCart = false;
+        removedProduct.count = 0;
+        removedProduct.total = 0;
+
+        setCart([...tempCart]);
+        setProducts([...tempProducts]);
+        addTotals();
+    };
+
+    const setOurProducts = async () => {
+        let ourProducts = [];
+        await storeProducts.forEach((item) => {
+            const singleItem = {...item};
+            ourProducts = [...ourProducts, singleItem];
+        });
+        await setProducts(ourProducts);
     };
 
     return (
