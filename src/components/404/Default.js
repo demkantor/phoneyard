6e4490a3514f48b130d8bb8408b72e2788f613f0
demkantor <demkantor@gmail.com>
2020-08-10 21:53:36 -0500

    cleaned up 404 page

--- src/components/404/Default.js
+++ src/components/404/Default.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
-const Default = () => {
+const Default = ( props ) => {
+    
     return (
-        <div>
-            <h3>page not found</h3>
+        <div className="container">
+            <div className="row">
+                <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+                    <h1 className="display-3">404</h1>
+                    <h1>Error</h1>
+                    <h2>page not found</h2>
+                    <h3>
+                        the requested URL
+                        <span className="text-danger">
+                            {props.location.pathname}
+                        </span>
+                        {" "}
+                        was not found
+                    </h3>
+                </div>
+                <h3 className="mx-auto text-center pt-5">use navbar to take you to products or cart</h3>
+            </div>
         </div>
     );
 };
